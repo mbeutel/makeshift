855f668dbdcd89df0eb7531cbc09304caae90614
Moritz Beutel <mbeutel@users.noreply.github.com>
2018-06-11 14:11:37 +0200

    - moved function-related metafunctions to new header <makeshift/functional.hpp>

--- include/makeshift/experimental/property.hpp
+++ include/makeshift/experimental/property.hpp
@@ -4,9 +4,10 @@
 
 
 #include <functional>  // for invoke()
-#include <type_traits> // for declval<>(), decay<>, is_invocable<>, is_member_object_pointer<>
+#include <type_traits> // for declval<>(), decay<>, is_member_object_pointer<>
 
 #include <makeshift/type_traits.hpp> // for can_apply<>
+#include <makeshift/functional.hpp>  // for is_callable<>
 #include <makeshift/utility.hpp>     // for define_flags<>
 #include <makeshift/tuple.hpp>
 #include <makeshift/metadata.hpp>    // for property_metadata<>
