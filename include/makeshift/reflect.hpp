9d8f046c3d4c07311207a6da6276e0256cc9922f
Moritz Beutel <mbeutel@users.noreply.github.com>
2018-09-14 14:49:40 +0200

    Drafts of `struct_equal_to<>`, `struct_less<>`, `struct_hash<>`

--- include/makeshift/reflect.hpp
+++ include/makeshift/reflect.hpp
@@ -24,6 +24,20 @@ template <typename T, T... Vs>
 }
 
 
+template <typename PtrT>
+    struct member_object_pointer_accessor;
+template <typename T, typename C, T C::* Pointer>
+    struct member_object_pointer_accessor<std::integral_constant<T C::*, Pointer>>
+{
+    constexpr member_object_pointer_accessor(void) noexcept = default;
+    constexpr member_object_pointer_accessor(std::integral_constant<T C::*, Pointer>) noexcept { }
+
+    static const T& operator ()(const C& instance) { return instance.*Pointer; }
+    static void operator ()(C& instance, const T& value) { instance.*Pointer = value; }
+    static void operator ()(C& instance, T&& value) { instance.*Pointer = std::move(value); }
+};
+
+
 } // namespace detail
 
 
@@ -84,6 +98,94 @@ template <typename T, typename MetadataTagT = reflection_metadata_tag>
     else
         return values_from_metadata<T, MetadataTagT>();
 }
+
+
+    //ᅟ
+    // Returns a tuple of member metadata objects describing a struct type.
+    //
+template <typename T, typename MetadataTagT = reflection_metadata_tag>
+    constexpr auto members_from_metadata(tag<T> = { }, MetadataTagT = { })
+{
+    static_assert(have_metadata_v<T, MetadataTagT>, "no metadata available for given type and tag");
+    
+    return metadata_of<T, MetadataTagT>.attributes
+        | tuple_filter(template_predicate_v<is_same_template, member_metadata>);
+}
+
+
+    //ᅟ
+    // Returns an accessor for the given member metadata object.
+    //
+template <typename AccessorsC, typename AttributesT>
+    constexpr auto member_accessor(const member_metadata<AccessorsC, AttributesT>& memberMetadata)
+{
+    using AccessorsTuple = apply<type_tuple, AccessorsC>;
+    auto memberObjectPointer = AccessorsTuple{ }
+        | tuple_filter(value_type_predicate{ predicate_v<std::is_member_object_pointer> })
+        | single_or_none();
+    if constexpr (!is_none_v<decltype(memberObjectPointer)>)
+        return makeshift::detail::member_object_pointer_accessor{ memberObjectPointer };
+}
+
+
+    //ᅟ
+    // Compares the two strings for equality using the given string comparison mode.
+    //
+template <typename T, typename MetadataTagT = reflection_metadata_tag>
+    class struct_equal_to
+{
+private:
+    static constexpr auto memberAccessors_ = metadata_of<T, MetadataTagT>.attributes
+        | tuple_filter(template_predicate_v<is_same_template, member_metadata>)
+        | tuple_map([](auto v) { return member_accessor(v); });
+
+public:
+    constexpr struct_equal_to(void) noexcept = default;
+    constexpr struct_equal_to(tag<T>, MetadataTagT = { }) noexcept = default;
+
+    template <typename LhsT, typename RhsT>
+        constexpr bool operator()(LhsT&& lhs, RhsT&& rhs) const noexcept
+    {
+        return memberAccessors_
+            | tuple_fold([&](auto&& accessor){  })
+    }
+};
+template <typename T>
+    class struct_equal_to(tag<T>) -> struct_equal_to<T>;
+template <typename T, typename MetadataTagT>
+    class struct_equal_to(tag<T>, const MetadataTagT&) -> struct_equal_to<T, MetadataTagT>;
+
+
+    //ᅟ
+    // Computes the hash of a string using the given string comparison mode.
+    //
+class struct_hash
+{
+private:
+    string_comparison comparison_;
+
+public:
+    constexpr string_hash(string_comparison _comparison) noexcept : comparison_(_comparison) { }
+
+    MAKESHIFT_DLLFUNC std::size_t operator()(std::string_view arg) const noexcept;
+};
+
+
+    //ᅟ
+    // Compares the two strings lexicographically using the given string comparison mode. Returns `true` if lower-case `lhs`
+    // lexicographically precedes lower-case `rhs`, `false` otherwise.
+    //
+class struct_less
+{
+private:
+    string_comparison comparison_;
+
+public:
+    constexpr string_less(string_comparison _comparison) noexcept : comparison_(_comparison) { }
+
+    MAKESHIFT_DLLFUNC bool operator()(std::string_view lhs, std::string_view rhs) const noexcept;
+};
+
 
 } // inline namespace metadata
 
