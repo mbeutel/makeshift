eeda62b94c7a4b2ed4caf45e3b0b97e691adcfc8
Moritz Beutel <mbeutel@users.noreply.github.com>
2018-09-20 21:52:48 +0200

    Expose `streamable_t<>` type, add specialization for rvalue reference

--- include/makeshift/streamable.hpp
+++ include/makeshift/streamable.hpp
@@ -12,8 +12,15 @@
 namespace makeshift
 {
 
-namespace detail
+inline namespace serialize
 {
+
+
+    // To customize stream serialization for arbitrary types, define your own serializer type along with `to_stream_impl()`, `from_stream_impl()`
+    // overloads discoverable by ADL.
+    //
+    // To override parts of the behavior of an existing serializer, define a new serializer that handles only the types you want to customize, and
+    // chain it with the existing serializer using `chain()`.
 
 
 template <typename T, typename SerializerT>
@@ -25,6 +32,29 @@ private:
 
 public:
     constexpr streamable_t(const T& _value, SerializerT _serializer) : value_(_value), serializer_(std::forward<SerializerT>(_serializer)) { }
+    constexpr streamable_t(T&& _value, SerializerT _serializer) : value_(std::move(_value)), serializer_(std::forward<SerializerT>(_serializer)) { }
+
+    friend std::ostream& operator <<(std::ostream& stream, const streamable_t& self)
+    {
+        to_stream_impl(self.value_, stream, self.serializer_, self.serializer_);
+        return stream;
+    }
+    friend std::istream& operator >>(std::istream& stream, const streamable_t& self)
+    {
+        from_stream_impl(self.value_, stream, self.serializer_, self.serializer_);
+        return stream;
+    }
+
+    constexpr const T& value(void) noexcept { return value_; }
+};
+template <typename T, typename SerializerT>
+    struct streamable_t<T&&, SerializerT>
+{
+private:
+    T value_;
+    SerializerT serializer_;
+
+public:
     constexpr streamable_t(T&& _value, SerializerT _serializer) : value_(std::move(_value)), serializer_(std::forward<SerializerT>(_serializer)) { }
 
     friend std::ostream& operator <<(std::ostream& stream, const streamable_t& self)
@@ -71,21 +101,7 @@ public:
     }
 };
 template <typename T, typename SerializerT>
-    streamable_t(T&& value, SerializerT&& serializer) -> streamable_t<remove_rvalue_reference_t<T>, remove_rvalue_reference_t<SerializerT>>;
-
-
-} // namespace detail
-
-
-inline namespace serialize
-{
-
-
-    // To customize stream serialization for arbitrary types, define your own serializer type along with `to_stream_impl()`, `from_stream_impl()`
-    // overloads discoverable by ADL.
-    //
-    // To override parts of the behavior of an existing serializer, define a new serializer that handles only the types you want to customize, and
-    // chain it with the existing serializer using `chain()`.
+    streamable_t(T&& value, SerializerT&& serializer) -> streamable_t<T, remove_rvalue_reference_t<SerializerT>>;
 
 
     //ᅟ
@@ -98,7 +114,7 @@ inline namespace serialize
 template <typename T, typename SerializerT>
     auto streamable(T&& value, SerializerT&& serializer)
 {
-    return makeshift::detail::streamable_t{ std::forward<T>(value), std::forward<SerializerT>(serializer) };
+    return streamable_t{ std::forward<T>(value), std::forward<SerializerT>(serializer) };
 }
 
 
