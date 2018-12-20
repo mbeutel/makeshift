fd5100c86a80c30ec51040ef6789aac266e96120
Moritz Beutel <mbeutel@users.noreply.github.com>
2018-12-20 19:36:51 +0100

    Add minor workaround for VS 2017 15.9

--- include/makeshift/detail/serializers_hint-reflect.hpp
+++ include/makeshift/detail/serializers_hint-reflect.hpp
@@ -26,8 +26,30 @@ namespace detail
 MAKESHIFT_PUBLIC void raw_string_to_stream(std::ostream& stream, std::string_view string);
 MAKESHIFT_PUBLIC void name_to_stream(std::ostream& stream, std::string_view name);
 
+template <typename MemberT, typename SerializerT>
+    void compound_member_type_hint_to_stream(SerializerT&, std::ostream& stream, const compound_hint_options& options)
+{
+    using MetadataTag = metadata_tag_of_serializer_t<std::decay_t<SerializerT>>;
+    if constexpr (!have_metadata_v<MemberT, MetadataTag>)
+        name_to_stream(stream, options.unnamed_member_placeholder);
+    else
+    {
+        auto slug = get_or_default<std::string_view>(metadata_of<MemberT, MetadataTag>.attributes);
+        if (!slug.empty())
+            name_to_stream(stream, slug);
+        else
+        {
+            auto caption = get_or_default<caption_metadata>(metadata_of<MemberT, MetadataTag>.attributes);
+            if (!caption.value.empty())
+                name_to_stream(stream, caption.value);
+            else
+                name_to_stream(stream, options.unnamed_member_placeholder);
+        }
+    }
+}
+
 template <typename T, typename SerializerT>
-    std::string get_compound_hint(SerializerT&, const any_compound_hint_options& compoundOptions)
+    std::string get_compound_hint(SerializerT& serializer, const any_compound_hint_options& compoundOptions)
 {
     using MetadataTag = metadata_tag_of_serializer_t<std::decay_t<SerializerT>>;
     constexpr auto members = members_of<T, MetadataTag>();
@@ -51,24 +73,7 @@ template <typename T, typename SerializerT>
         if (!theName.empty())
             name_to_stream(sstr, theName);
         else
-        {
-            if constexpr (!have_metadata_v<Member, MetadataTag>)
-                name_to_stream(sstr, options.unnamed_member_placeholder);
-            else
-            {
-                auto slug = get_or_default<std::string_view>(metadata_of<Member, MetadataTag>.attributes);
-                if (!slug.empty())
-                    name_to_stream(sstr, slug);
-                else
-                {
-                    auto caption = get_or_default<caption_metadata>(metadata_of<Member, MetadataTag>.attributes);
-                    if (!caption.value.empty())
-                        name_to_stream(sstr, caption.value);
-                    else
-                        name_to_stream(sstr, options.unnamed_member_placeholder);
-                }
-            }
-        }
+            compound_member_type_hint_to_stream<Member>(serializer, sstr, options);
     });
     raw_string_to_stream(sstr, options.closing_delimiter);
     return sstr.str();
