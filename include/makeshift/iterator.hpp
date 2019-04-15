c7a99c5d757c5904096f3a86b52b826ec5c63df8
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-04-15 13:26:36 +0200

    Add `callback_input_iterator<>`

--- include/makeshift/iterator.hpp
+++ include/makeshift/iterator.hpp
@@ -14,6 +14,50 @@ namespace makeshift
 
 
 template <typename FuncT>
+    class callback_input_iterator
+{
+public:
+    using difference_type = void;
+    using value_type = void;
+    using pointer = void;
+    using reference = decltype(std::declval<FuncT>()());
+    using iterator_category = std::input_iterator_tag;
+
+private:
+    FuncT func_;
+
+public:
+    constexpr explicit callback_input_iterator(FuncT _func)
+        : func_{ std::move(_func) }
+    {
+    }
+
+    constexpr reference operator *(void)
+    {
+        return func_();
+    }
+
+        // We don't bother to check if dereference--assign statements and increment statements are sequenced alternatingly as required.
+    constexpr callback_input_iterator& operator ++(void)
+    {
+        return *this;
+    }
+    constexpr callback_input_iterator& operator ++(int)
+    {
+        return *this;
+    }
+};
+template <typename FuncT>
+    callback_input_iterator(FuncT&&) -> callback_input_iterator<std::decay_t<FuncT>>;
+
+template <typename FuncT>
+    MAKESHIFT_NODISCARD constexpr callback_input_iterator<std::decay_t<FuncT>> make_callback_input_iterator(FuncT&& func)
+{
+	return callback_input_iterator<std::decay_t<FuncT>>{ std::forward<FuncT>(func) };
+}
+
+
+template <typename FuncT>
     class callback_output_iterator
 {
 public:
