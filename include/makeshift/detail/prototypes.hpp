c45ae586bc8964d0acdb17cd701fa1b2e5cfabe4
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-05-14 15:54:09 +0200

    Add prototypes headers

---
+++ include/makeshift/detail/prototypes.hpp
@@ -0,0 +1,21 @@
+
+#ifndef INCLUDED_MAKESHIFT_DETAIL_PROTOTYPES_HPP_
+#define INCLUDED_MAKESHIFT_DETAIL_PROTOTYPES_HPP_
+
+
+
+namespace makeshift
+{
+
+namespace detail
+{
+
+
+
+
+} // namespace detail
+
+} // namespace makeshift
+
+
+#endif // INCLUDED_MAKESHIFT_DETAIL_PROTOTYPES_HPP_
---
+++ include/makeshift/prototypes.hpp
@@ -0,0 +1,22 @@
+
+#ifndef INCLUDED_MAKESHIFT_PROTOTYPES_HPP_
+#define INCLUDED_MAKESHIFT_PROTOTYPES_HPP_
+
+
+#include <type_traits> // for integral_constant<>
+
+#include <makeshift/version.hpp> // for MAKESHIFT_NODISCARD
+
+#include <makeshift/detail/prototypes.hpp>
+
+
+namespace makeshift
+{
+
+
+
+
+} // namespace makeshift
+
+
+#endif // INCLUDED_MAKESHIFT_PROTOTYPES_HPP_
