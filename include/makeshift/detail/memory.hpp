29519459d1c71f7133331aec95cb2b42ce5461fa
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-07-16 16:46:20 +0200

    Add `allocate_unique<>()` and class `allocator_deleter<>`

---
+++ include/makeshift/detail/memory.hpp
@@ -0,0 +1,93 @@
+
+#ifndef INCLUDED_MAKESHIFT_DETAIL_MEMORY_HPP_
+#define INCLUDED_MAKESHIFT_DETAIL_MEMORY_HPP_
+
+
+#include <cstddef>     // for size_t, ptrdiff_t
+#include <type_traits> // for integral_constant<>
+#include <memory>      // for allocator_traits<>
+
+
+namespace makeshift
+{
+
+namespace detail
+{
+
+
+template <typename T> struct remove_extent_only;
+template <typename T> struct remove_extent_only<T[]> { using type = T; };
+template <typename T, std::ptrdiff_t N> struct remove_extent_only<T[N]> { using type = T; };
+template <typename T> using remove_extent_only_t = typename remove_extent_only<T>::type;
+
+template <typename T> struct extent_only;
+template <typename T> struct extent_only<T[]> : std::integral_constant<std::ptrdiff_t, 0> { };
+template <typename T, std::ptrdiff_t N> struct extent_only<T[N]> : std::integral_constant<std::ptrdiff_t, N> { };
+template <typename T> constexpr std::ptrdiff_t extent_only_v = extent_only<T>::value;
+
+
+template <typename T, typename A, typename... ArgsT>
+    T* allocate(std::true_type /*isNothrowConstructible*/, A& alloc, ArgsT&&... args)
+{
+    T* ptr = std::allocator_traits<A>::allocate(alloc, 1);
+    std::allocator_traits<A>::construct(alloc, ptr, std::forward<ArgsT>(args)...);
+    return ptr;
+}
+template <typename T, typename A, typename... ArgsT>
+    T* allocate(std::false_type /*isNothrowConstructible*/, A& alloc, ArgsT&&... args)
+{
+    T* ptr = std::allocator_traits<A>::allocate(alloc, 1);
+    try
+    {
+        std::allocator_traits<A>::construct(alloc, ptr, std::forward<ArgsT>(args)...);
+    }
+    catch (...)
+    {
+        std::allocator_traits<A>::deallocate(alloc, ptr, 1);
+        throw;
+    }
+    return ptr;
+}
+
+template <typename T, typename A, typename SizeC>
+    T* allocate_array(std::true_type /*isNothrowDefaultConstructible*/, A& alloc, SizeC sizeC)
+{
+    T* ptr = std::allocator_traits<A>::allocate(alloc, std::size_t(sizeC));
+    for (std::ptrdiff_t i = 0; i != std::ptrdiff_t(sizeC); ++i)
+    {
+        std::allocator_traits<A>::construct(alloc, &ptr[i]);
+    }
+    return ptr;
+}
+template <typename T, typename A, typename SizeC>
+    T* allocate_array(std::false_type /*isNothrowDefaultConstructible*/, A& alloc, SizeC sizeC)
+{
+    T* ptr = std::allocator_traits<A>::allocate(alloc, std::size_t(sizeC));
+    std::ptrdiff_t i = 0;
+    try
+    {
+        for (; i != std::ptrdiff_t(sizeC); ++i)
+        {
+            std::allocator_traits<A>::construct(alloc, &ptr[i]);
+        }
+    }
+    catch (...)
+    {
+            // Revert by destroying all already-constructed items, deallocating, and then re-throwing the exception.
+        for (--i; i >= 0; --i)
+        {
+            std::allocator_traits<A>::destroy(alloc, &ptr[i]);
+        }
+        std::allocator_traits<A>::deallocate(alloc, ptr, std::size_t(sizeC));
+        throw;
+    }
+    return ptr;
+}
+
+
+} // namespace detail
+
+} // namespace makeshift
+
+
+#endif // INCLUDED_MAKESHIFT_DETAIL_MEMORY_HPP_
--- include/makeshift/memory.hpp
+++ include/makeshift/memory.hpp
@@ -1,19 +1,25 @@
-
+﻿
 #ifndef INCLUDED_MAKESHIFT_MEMORY_HPP_
 #define INCLUDED_MAKESHIFT_MEMORY_HPP_
 
 
 #include <new>
-#include <memory>      // for allocator<>, allocator_traits<>
-#include <type_traits> // for is_nothrow_default_constructible<>
+#include <cstddef>     // for size_t, ptrdiff_t
+#include <memory>      // for unique_ptr<>, allocator<>, allocator_traits<>
+#include <utility>     // for forward<>()
+#include <type_traits> // for is_nothrow_default_constructible<>, enable_if<>, is_same<>, remove_cv<>
+
+#include <makeshift/detail/memory.hpp>
 
 
 namespace makeshift
 {
 
 
+    //ᅟ
     // Allocator adaptor that interposes construct() calls to convert value initialization into default initialization.
     // cf. https://stackoverflow.com/a/21028912
+    //
 template <typename T, typename A = std::allocator<T>>
     class default_init_allocator : public A
 {
@@ -38,6 +44,131 @@ public:
         std::allocator_traits<A>::construct(static_cast<A&>(*this), ptr, std::forward<ArgsT>(args)...);
     }
 };
+
+
+    //ᅟ
+    // Deleter for `std::unique_ptr<>` which supports custom allocators.
+    //ᅟ
+    //ᅟ    auto p1 = allocate_unique<float>(MyAllocator<float>{ }, 3.14159f); // returns `std::unique_ptr<float, allocator_deleter<float, MyAllocator<float>>>`
+    //ᅟ    auto p2 = allocate_unique<float[]>(MyAllocator<float>{ }, 42); // returns `std::unique_ptr<float[], allocator_deleter<float[], MyAllocator<float>>>`
+    //ᅟ    auto p3 = allocate_unique<float[42]>(MyAllocator<float>{ }); // returns `std::unique_ptr<float[42], allocator_deleter<float[42], MyAllocator<float>>>`
+    //
+template <typename T, typename A>
+    class allocator_deleter
+{
+private:
+    A alloc_;
+
+public:
+    allocator_deleter(const A& _alloc)
+        : alloc_(_alloc)
+    {
+    }
+    void operator ()(T* ptr)
+    {
+        std::allocator_traits<A>::destroy(alloc_, ptr);
+        std::allocator_traits<A>::deallocate(alloc_, ptr, 1);
+    }
+};
+
+template <typename T, typename A>
+    class allocator_deleter<T[], A>
+{
+private:
+    A alloc_;
+    std::size_t size_;
+
+public:
+    allocator_deleter(const A& _alloc, std::size_t _size)
+        : alloc_(_alloc), size_(_size)
+    {
+    }
+    void operator ()(T* ptr)
+    {
+        for (std::ptrdiff_t i = 0, n = std::ptrdiff_t(size_); i != n; ++i)
+        {
+            std::allocator_traits<A>::destroy(alloc_, &ptr[i]);
+        }
+        std::allocator_traits<A>::deallocate(alloc_, ptr, size_);
+    }
+};
+
+template <typename T, std::ptrdiff_t N, typename A>
+    class allocator_deleter<T[N], A>
+{
+private:
+    A alloc_;
+
+public:
+    allocator_deleter(const A& _alloc)
+        : alloc_(_alloc)
+    {
+    }
+    void operator ()(T* ptr)
+    {
+        for (std::ptrdiff_t i = 0; i != N; ++i)
+        {
+            std::allocator_traits<A>::destroy(alloc_, &ptr[i]);
+        }
+        std::allocator_traits<A>::deallocate(alloc_, ptr, N);
+    }
+};
+
+
+    //ᅟ
+    // Allocates an object of type `T` with the given allocator, constructs it with the given arguments and returns a `std::unique_ptr<>` to the object.
+    //ᅟ
+    //ᅟ    auto p = allocate_unique<float>(MyAllocator<float>{ }, 3.14159f);
+    //ᅟ    // returns `std::unique_ptr<float, allocator_deleter<float, MyAllocator<float>>>`
+    //
+template <typename T, typename A, typename... ArgsT,
+          std::enable_if_t<!can_instantiate_v<makeshift::detail::remove_extent_only_t, T>, int> = 0>
+    std::unique_ptr<T, allocator_deleter<T, A>>
+    allocate_unique(A alloc, ArgsT&&... args)
+{
+    using NCVT = std::remove_cv_t<T>;
+    static_assert(std::is_same<typename std::allocator_traits<A>::value_type, NCVT>::value, "allocator has mismatching value_type");
+
+    NCVT* ptr = makeshift::detail::allocate<NCVT>(std::is_nothrow_constructible<NCVT, ArgsT...>{ }, alloc, std::forward<ArgsT>(args)...);
+    return { ptr, { std::move(alloc) } };
+}
+
+    //ᅟ
+    // Allocates a fixed-size array of type `ArrayT` with the given allocator, default-constructs the elements and returns a `std::unique_ptr<>` to the array.
+    //ᅟ
+    //ᅟ    auto p = allocate_unique<float[42]>(MyAllocator<float>{ });
+    //ᅟ    // returns `std::unique_ptr<float[42], allocator_deleter<float[42], MyAllocator<float>>>`
+    //
+template <typename ArrayT, typename A,
+          std::enable_if_t<makeshift::detail::extent_only_v<ArrayT> != 0, int> = 0>
+    std::unique_ptr<ArrayT, allocator_deleter<ArrayT, A>>
+    allocate_unique(A alloc)
+{
+    using T = std::remove_cv_t<makeshift::detail::remove_extent_only_t<ArrayT>>;
+    static_assert(std::is_same<typename std::allocator_traits<A>::value_type, T>::value, "allocator has mismatching value_type");
+
+    T* ptr = makeshift::detail::allocate_array<T>(std::is_nothrow_default_constructible<T>{ }, alloc, makeshift::detail::extent_only<ArrayT>{ });
+    return { ptr, { std::move(alloc) } };
+}
+
+    //ᅟ
+    // Allocates an array of type `ArrayT` with the given allocator, default-constructs the elements and returns a `std::unique_ptr<>` to the array.
+    //ᅟ
+    //ᅟ    auto p = allocate_unique<float[]>(MyAllocator<float>{ }, 42);
+    //ᅟ    // returns `std::unique_ptr<float[], allocator_deleter<float[], MyAllocator<float>>>`
+    //
+template <typename ArrayT, typename A,
+          std::enable_if_t<makeshift::detail::extent_only_v<ArrayT> == 0, int> = 0>
+    std::unique_ptr<ArrayT, allocator_deleter<ArrayT, A>>
+    allocate_unique(A alloc, std::size_t size)
+{
+    using T = std::remove_cv_t<makeshift::detail::remove_extent_only_t<ArrayT>>;
+    static_assert(std::is_same<typename std::allocator_traits<A>::value_type, T>::value, "allocator has mismatching value_type");
+
+    T* ptr = makeshift::detail::allocate_array<T>(std::is_nothrow_default_constructible<T>{ }, alloc, size);
+    return { ptr, { std::move(alloc), size } };
+}
+
 
 
 } // namespace makeshift
