419f0fb1c9da88e48265e24b8bbb1d2490d657b6
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-07-24 18:26:56 +0200

    Type enum's underlying enum now defines appropriate number of enumerators
    This permits us to make use of compiler warnings for non-exhaustive
    switch statements. (It also helps avoid VC++ warning C4063 "case
    '<integer>' is not a valid value for switch of enum".)

--- include/makeshift/detail/utility2.hpp
+++ include/makeshift/detail/utility2.hpp
@@ -81,8 +81,21 @@ protected:
 template <typename TypeEnumT, typename... Ts>
     struct define_type_enum_base : makeshift::detail::type_enum_base
 {
+        // TODO: it may be desirable to relax this requirement for reasons of generality
+    static_assert(sizeof...(Ts) > 0, "type enumeration must contain at least one type");
+
 private:
-    enum class value_t : int { };
+    static constexpr int n = sizeof...(Ts);
+
+        // This type exists to permit usage of type enums in switch statements. We define enumerators with suitable values to make use of compiler warnings
+        // for non-exhaustive switch statements. (We also avoid warning C4063 "case '<integer>' is not a valid value for switch of enum" for VC++ this way.)
+    enum class value_t : int
+    {
+        type0  = 0,               type1  = n >  1 ?  1 : 0, type2  = n >  2 ?  2 : 0, type3  = n >  3 ?  3 : 0, type4  = n >  4 ?  4 : 0, type5  = n >  5 ?  5 : 0,
+        type6  = n >  6 ?  6 : 0, type7  = n >  7 ?  7 : 0, type8  = n >  8 ?  8 : 0, type9  = n >  9 ?  9 : 0, type10 = n > 10 ? 10 : 0, type11 = n > 11 ? 11 : 0,
+        type12 = n > 12 ? 12 : 0, type13 = n > 13 ? 13 : 0, type14 = n > 14 ? 14 : 0, type15 = n > 15 ? 15 : 0, type16 = n > 16 ? 16 : 0, type17 = n > 17 ? 17 : 0,
+        type18 = n > 18 ? 18 : 0, type19 = n > 19 ? 19 : 0, type20 = n > 20 ? 20 : 0, type21 = n > 21 ? 21 : 0, type22 = n > 22 ? 22 : 0, type23 = n > 23 ? 23 : 0
+    };
 
     value_t value_;
 
