dcd985e93cb24eae47fc1278240242d63357c957
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-09-01 00:25:37 +0200

    Make buffer.hpp standalone

--- include/makeshift/buffer.hpp
+++ include/makeshift/buffer.hpp
@@ -6,12 +6,10 @@
 #include <array>
 #include <utility>     // for tuple_size<>, tuple_element<>
 #include <iterator>    // for move_iterator<>
+#include <algorithm>   // for copy()
 #include <type_traits> // for is_convertible<>
 
 #include <gsl/gsl-lite.hpp> // for Expects()
-
-#include <makeshift/constval.hpp> // for constval_extract()
-#include <makeshift/utility2.hpp> // for dim
 
 #include <makeshift/detail/buffer.hpp>
 
@@ -20,48 +18,35 @@ namespace makeshift
 {
 
 
-constexpr dim dynamic_extent = -1;
+constexpr std::ptrdiff_t dynamic_extent = -1;
 
 
-template <typename C>
-    constexpr dim extent_from_constval(C = { })
-{
-    return makeshift::detail::extent_from_constval_impl<C>(is_constval<C>{ });
-}
-
-
-template <typename T, dim Extent = dynamic_extent, dim MaxStaticBufferExtent = dynamic_extent>
+template <typename T, std::ptrdiff_t Extent = dynamic_extent, std::ptrdiff_t MaxStaticBufferExtent = dynamic_extent>
     class buffer
         : public makeshift::detail::buffer_base<T, Extent, MaxStaticBufferExtent, makeshift::detail::determine_memory_location(Extent, MaxStaticBufferExtent)>
 {
 private:
-    using _base = makeshift::detail::buffer_base<T, Extent, MaxStaticBufferExtent, makeshift::detail::determine_memory_location(Extent, MaxStaticBufferExtent)>;
-
-    struct check_size_functor
-    {
-        constexpr bool operator ()(dim size) const noexcept
-        {
-            return size >= 0
-                && (Extent == dynamic_extent || size == Extent);
-        }
-    };
+    using base_ = makeshift::detail::buffer_base<T, Extent, MaxStaticBufferExtent, makeshift::detail::determine_memory_location(Extent, MaxStaticBufferExtent)>;
 
 public:
-    template <typename C>
-        explicit constexpr buffer(C _size)
-            : _base(makeshift::constval_extract(_size))
+    template <typename ExtentC,
+              std::enable_if_t<std::is_convertible<ExtentC, std::size_t>::value, int> = 0>
+        explicit constexpr buffer(ExtentC extent)
+            : base_(extent)
     {
-        makeshift::constval_assert(check_size_functor{ }, _size); // ensure that static and dynamic sizes match
+        constexpr std::ptrdiff_t rhsExtent = makeshift::detail::buffer_extent_from_constval(ExtentC{ });
+        static_assert(Extent == dynamic_extent || rhsExtent == -1 || Extent == rhsExtent, "static extents must match");
+        makeshift::detail::check_buffer_extents(std::integral_constant<bool, Extent == dynamic_extent>{ }, Extent, extent);
     }
-    template <dim RExtent, typename U>
+    template <std::ptrdiff_t RExtent, typename U>
         constexpr buffer(U (&&array)[RExtent])
-            : _base(RExtent)
+            : base_(RExtent)
     {
         static_assert(Extent == dynamic_extent || RExtent == Extent, "array extent does not match");
         static_assert(std::is_convertible<U, T>::value, "incompatible array element types");
         std::copy(std::make_move_iterator(array), std::make_move_iterator(array + RExtent), this->begin());
     }
-    template <dim RExtent, typename U>
+    template <std::ptrdiff_t RExtent, typename U>
         constexpr buffer& operator =(U (&&array)[RExtent])
     {
         static_assert(Extent == dynamic_extent || RExtent == Extent, "array extent does not match");
@@ -75,23 +60,23 @@ public:
 template <typename T,
           typename C>
     MAKESHIFT_NODISCARD constexpr
-    buffer<T, makeshift::extent_from_constval<C>()>
+    buffer<T, makeshift::detail::buffer_extent_from_constval(C{ })>
     make_buffer(C size)
 {
-    return buffer<T, makeshift::extent_from_constval<C>()>{ size };
+    return buffer<T, makeshift::detail::buffer_extent_from_constval(C{ })>(size);
 }
 
-template <typename T, dim MaxStaticBufferExtent,
+template <typename T, std::ptrdiff_t MaxStaticBufferExtent,
           typename C>
     MAKESHIFT_NODISCARD constexpr
-    buffer<T, makeshift::extent_from_constval<C>(), MaxStaticBufferExtent>
+    buffer<T, makeshift::detail::buffer_extent_from_constval(C{ }), MaxStaticBufferExtent>
     make_buffer(C size)
 {
-    return buffer<T, makeshift::extent_from_constval<C>(), MaxStaticBufferExtent>{ size };
+    return buffer<T, makeshift::detail::buffer_extent_from_constval(C{ }), MaxStaticBufferExtent>(size);
 }
 
 
-template <typename T, dim Extent, dim MaxBufferExtent>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxBufferExtent>
     class fixed_buffer
         : public makeshift::detail::static_buffer_base<T, Extent, MaxBufferExtent>
 {
@@ -99,34 +84,28 @@ template <typename T, dim Extent, dim MaxBufferExtent>
     static_assert(Extent <= MaxBufferExtent, "size exceeds buffer extent");
 
 private:
-    using _base = makeshift::detail::static_buffer_base<T, Extent, MaxBufferExtent>;
-
-    struct check_size_functor
-    {
-        constexpr bool operator ()(dim size) const noexcept
-        {
-            return size >= 0
-                && (Extent == dynamic_extent || size == Extent)
-                && size <= MaxBufferExtent;
-        }
-    };
+    using base_ = makeshift::detail::static_buffer_base<T, Extent, MaxBufferExtent>;
 
 public:
-    template <typename C>
-        explicit constexpr fixed_buffer(C _size)
-            : _base(makeshift::constval_extract(_size))
+    template <typename ExtentC,
+              std::enable_if_t<std::is_convertible<ExtentC, std::size_t>::value, int> = 0>
+        explicit constexpr fixed_buffer(ExtentC extent)
+            : base_(extent)
     {
-        makeshift::constval_assert(check_size_functor{ }, _size); // ensure that static and dynamic sizes match
+        constexpr std::ptrdiff_t rhsExtent = makeshift::detail::buffer_extent_from_constval(ExtentC{ });
+        static_assert(Extent == dynamic_extent || rhsExtent == -1 || Extent == rhsExtent, "static extents must match");
+        static_assert(rhsExtent == -1 || rhsExtent <= MaxBufferExtent, "size exceeds buffer extent");
+        makeshift::detail::check_fixed_buffer_extents(std::integral_constant<bool, Extent == dynamic_extent>{ }, Extent, extent, MaxBufferExtent);
     }
-    template <dim RExtent, typename U>
+    template <std::ptrdiff_t RExtent, typename U>
         constexpr fixed_buffer(U (&&array)[RExtent])
-            : _base(RExtent)
+            : base_(RExtent)
     {
         static_assert(Extent == dynamic_extent || RExtent == Extent, "array extent does not match");
         static_assert(std::is_convertible<U, T>::value, "incompatible array element types");
         std::copy(std::make_move_iterator(array), std::make_move_iterator(array + RExtent), this->begin());
     }
-    template <dim RExtent, typename U>
+    template <std::ptrdiff_t RExtent, typename U>
         constexpr fixed_buffer& operator =(U (&&array)[RExtent])
     {
         static_assert(Extent == dynamic_extent || RExtent == Extent, "array extent does not match");
@@ -137,51 +116,51 @@ public:
     }
 };
 
-template <typename T, typename C>
+template <typename T, typename SizeT, SizeT Size>
     MAKESHIFT_NODISCARD constexpr
-    fixed_buffer<T, makeshift::extent_from_constval<C>(), makeshift::detail::constval_value<C>>
-    make_fixed_buffer(C size)
+    fixed_buffer<T, Size, Size>
+    make_fixed_buffer(std::integral_constant<SizeT, Size>)
 {
-    static_assert(is_constval_v<C>, "fixed buffer size must be a constval if no maximal extent is given");
-    return fixed_buffer<T, makeshift::extent_from_constval<C>(), makeshift::detail::constval_value<C>>{ size };
+    return fixed_buffer<T, Size, Size>(std::integral_constant<SizeT, Size>{ });
 }
 
-template <typename T, dim MaxBufferExtent, typename C>
+template <typename T, std::ptrdiff_t MaxBufferExtent,
+          typename C>
     MAKESHIFT_NODISCARD constexpr
-    fixed_buffer<T, makeshift::extent_from_constval<C>(), MaxBufferExtent>
+    fixed_buffer<T, makeshift::detail::buffer_extent_from_constval(C{ }), MaxBufferExtent>
     make_fixed_buffer(C size)
 {
-    return fixed_buffer<T, makeshift::extent_from_constval<C>(), MaxBufferExtent>{ size };
+    return fixed_buffer<T, makeshift::detail::buffer_extent_from_constval(C{ }), MaxBufferExtent>(size);
 }
 
 
     // Implement tuple-like protocol for `buffer<>`.
-template <std::size_t I, typename T, dim Extent, dim MaxStaticBufferExtent,
-          std::enable_if_t<Extent != dynamic_extent, int> = 0>
-    MAKESHIFT_NODISCARD constexpr T& get(buffer<T, Extent, MaxStaticBufferExtent>& buffer) noexcept
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent>
+    MAKESHIFT_NODISCARD constexpr std::enable_if_t<Extent != dynamic_extent, T&>
+    get(buffer<T, Extent, MaxStaticBufferExtent>& buffer) noexcept
 {
     static_assert(I < Extent, "index out of range");
     return buffer[I];
 }
-template <std::size_t I, typename T, dim Extent, dim MaxStaticBufferExtent,
-          std::enable_if_t<Extent != dynamic_extent, int> = 0>
-    MAKESHIFT_NODISCARD constexpr const T& get(const buffer<T, Extent, MaxStaticBufferExtent>& buffer) noexcept
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent>
+    MAKESHIFT_NODISCARD constexpr std::enable_if_t<Extent != dynamic_extent, T const&>
+    get(buffer<T, Extent, MaxStaticBufferExtent> const& buffer) noexcept
 {
     static_assert(I < Extent, "index out of range");
     return buffer[I];
 }
 
     // Implement tuple-like protocol for `fixed_buffer<>`.
-template <std::size_t I, typename T, dim Extent, dim MaxBufferExtent,
-          std::enable_if_t<Extent != dynamic_extent, int> = 0>
-    MAKESHIFT_NODISCARD constexpr T& get(fixed_buffer<T, Extent, MaxBufferExtent>& buffer) noexcept
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxBufferExtent>
+    MAKESHIFT_NODISCARD constexpr std::enable_if_t<Extent != dynamic_extent, T&>
+    get(fixed_buffer<T, Extent, MaxBufferExtent>& buffer) noexcept
 {
     static_assert(I < Extent, "index out of range");
     return buffer[I];
 }
-template <std::size_t I, typename T, dim Extent, dim MaxBufferExtent,
-          std::enable_if_t<Extent != dynamic_extent, int> = 0>
-    MAKESHIFT_NODISCARD constexpr const T& get(const fixed_buffer<T, Extent, MaxBufferExtent>& buffer) noexcept
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxBufferExtent>
+    MAKESHIFT_NODISCARD constexpr std::enable_if_t<Extent != dynamic_extent, T const&>
+    get(fixed_buffer<T, Extent, MaxBufferExtent> const& buffer) noexcept
 {
     static_assert(I < Extent, "index out of range");
     return buffer[I];
@@ -196,14 +175,14 @@ namespace std
 
 
     // Implement tuple-like protocol for `buffer<>`.
-template <typename T, makeshift::dim Extent, makeshift::dim MaxStaticBufferExtent> struct tuple_size<makeshift::buffer<T, Extent, MaxStaticBufferExtent>> : std::integral_constant<std::size_t, Extent> { };
-template <typename T, makeshift::dim MaxStaticBufferExtent> struct tuple_size<makeshift::buffer<T, -1, MaxStaticBufferExtent>>; // undefined
-template <std::size_t I, typename T, makeshift::dim Extent, makeshift::dim MaxStaticBufferExtent> struct tuple_element<I, makeshift::buffer<T, Extent, MaxStaticBufferExtent>> { using type = T; };
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent> struct tuple_size<makeshift::buffer<T, Extent, MaxStaticBufferExtent>> : std::integral_constant<std::size_t, Extent> { };
+template <typename T, std::ptrdiff_t MaxStaticBufferExtent> struct tuple_size<makeshift::buffer<T, -1, MaxStaticBufferExtent>>; // undefined
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent> struct tuple_element<I, makeshift::buffer<T, Extent, MaxStaticBufferExtent>> { using type = T; };
 
     // Implement tuple-like protocol for `fixed_buffer<>`.
-template <typename T, makeshift::dim Extent, makeshift::dim MaxBufferExtent> struct tuple_size<makeshift::fixed_buffer<T, Extent, MaxBufferExtent>> : std::integral_constant<std::size_t, Extent> { };
-template <typename T, makeshift::dim MaxBufferExtent> struct tuple_size<makeshift::fixed_buffer<T, -1, MaxBufferExtent>>; // undefined
-template <std::size_t I, typename T, makeshift::dim Extent, makeshift::dim MaxBufferExtent> struct tuple_element<I, makeshift::fixed_buffer<T, Extent, MaxBufferExtent>> { using type = T; };
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxBufferExtent> struct tuple_size<makeshift::fixed_buffer<T, Extent, MaxBufferExtent>> : std::integral_constant<std::size_t, Extent> { };
+template <typename T, std::ptrdiff_t MaxBufferExtent> struct tuple_size<makeshift::fixed_buffer<T, -1, MaxBufferExtent>>; // undefined
+template <std::size_t I, typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxBufferExtent> struct tuple_element<I, makeshift::fixed_buffer<T, Extent, MaxBufferExtent>> { using type = T; };
 
 
 } // namespace std
--- include/makeshift/detail/buffer.hpp
+++ include/makeshift/detail/buffer.hpp
@@ -5,13 +5,12 @@
 
 #include <array>
 #include <cstddef>     // for size_t, ptrdiff_t
+#include <algorithm>   // for copy()
 #include <type_traits> // for integral_constant<>
 
 #include <gsl/gsl-lite.hpp> // for Expects()
 
-#include <makeshift/utility2.hpp> // for dim
-
-#include <makeshift/detail/constval.hpp> // for constval_value<>
+#include <makeshift/macros.hpp> // for MAKESHIFT_NODISCARD, MAKESHIFT_CONSTEXPR17
 
 
 namespace makeshift
@@ -29,32 +28,32 @@ public:
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     using pointer = T*;
-    using const_pointer = const T*;
+    using const_pointer = T const*;
     using reference = T&;
-    using const_reference = const T&;
+    using const_reference = T const&;
 
     using iterator = T*;
-    using const_iterator = const T*;
+    using const_iterator = T const*;
 
 private:
-    MAKESHIFT_CONSTEXPR_CXX17 T* _data(void) noexcept { return static_cast<DerivedT*>(this)->data(); }
-    MAKESHIFT_CONSTEXPR_CXX17 const T* _data(void) const noexcept { return static_cast<const DerivedT*>(this)->data(); }
-    MAKESHIFT_CONSTEXPR_CXX17 std::size_t _size(void) const noexcept { return static_cast<const DerivedT*>(this)->size(); }
+    MAKESHIFT_CONSTEXPR17 T* _data(void) noexcept { return static_cast<DerivedT*>(this)->data(); }
+    MAKESHIFT_CONSTEXPR17 T const* _data(void) const noexcept { return static_cast<DerivedT const*>(this)->data(); }
+    MAKESHIFT_CONSTEXPR17 std::size_t _size(void) const noexcept { return static_cast<DerivedT const*>(this)->size(); }
 
 public:
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 iterator begin(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 iterator begin(void) noexcept
     {
         return _data();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_iterator begin(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_iterator begin(void) const noexcept
     {
         return _data();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 iterator end(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 iterator end(void) noexcept
     {
         return _data() + _size();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_iterator end(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_iterator end(void) const noexcept
     {
         return _data() + _size();
     }
@@ -64,48 +63,48 @@ public:
         return _size() == 0;
     }
 
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 reference at(size_type i)
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 reference at(size_type i)
     {
         Expects(i < _size());
         return _data()[i];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_reference at(size_type i) const
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_reference at(size_type i) const
     {
         Expects(i < _size());
         return _data()[i];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 reference operator [](size_type i)
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 reference operator [](size_type i)
     {
         return _data()[i];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_reference operator [](size_type i) const
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_reference operator [](size_type i) const
     {
         return _data()[i];
     }
 
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 reference front(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 reference front(void) noexcept
     {
         Expects(!empty());
         return _data()[0];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_reference front(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_reference front(void) const noexcept
     {
         Expects(!empty());
         return _data()[0];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 reference back(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 reference back(void) noexcept
     {
         Expects(!empty());
         return _data()[_size() - 1];
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_reference back(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_reference back(void) const noexcept
     {
         Expects(!empty());
         return _data()[_size() - 1];
     }
 };
 
-template <typename T, dim Extent, dim MaxExtent = -1>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxExtent = -1>
     class static_buffer_base : public buffer_interface_mixin<T, static_buffer_base<T, Extent, MaxExtent>>
 {
     static_assert(Extent >= 0, "buffer extent must be non-negative");
@@ -122,19 +121,19 @@ public:
     {
     }
 
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 iterator begin(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 iterator begin(void) noexcept
     {
         return data_.begin();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_iterator begin(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_iterator begin(void) const noexcept
     {
         return data_.begin();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 iterator end(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 iterator end(void) noexcept
     {
         return data_.end();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const_iterator end(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 const_iterator end(void) const noexcept
     {
         return data_.end();
     }
@@ -144,16 +143,16 @@ public:
         return Extent;
     }
 
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 T* data(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 T* data(void) noexcept
     {
         return data_.data();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const T* data(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 T const* data(void) const noexcept
     {
         return data_.data();
     }
 };
-template <typename T, dim MaxExtent>
+template <typename T, std::ptrdiff_t MaxExtent>
     class static_buffer_base<T, -1, MaxExtent> : public buffer_interface_mixin<T, static_buffer_base<T, -1, MaxExtent>>
 {
 private:
@@ -171,17 +170,17 @@ public:
         return size_;
     }
 
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 T* data(void) noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 T* data(void) noexcept
     {
         return data_.data();
     }
-    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR_CXX17 const T* data(void) const noexcept
+    MAKESHIFT_NODISCARD MAKESHIFT_CONSTEXPR17 T const* data(void) const noexcept
     {
         return data_.data();
     }
 };
 
-template <typename T, dim BufExtent>
+template <typename T, std::ptrdiff_t BufExtent>
     class dynamic_buffer_base : public buffer_interface_mixin<T, dynamic_buffer_base<T, BufExtent>>
 {
 private:
@@ -198,13 +197,13 @@ public:
         else
             data_ = new T[_size]{ };
     }
-    constexpr dynamic_buffer_base(const dynamic_buffer_base& rhs)
+    constexpr dynamic_buffer_base(dynamic_buffer_base const& rhs)
         : dynamic_buffer_base(rhs.size_)
     {
         // TODO: we could use non-initializing allocation and unitialized_copy() to optimize this further
         std::copy(rhs.begin(), rhs.end(), data_);
     }
-    constexpr dynamic_buffer_base& operator =(const dynamic_buffer_base& rhs)
+    constexpr dynamic_buffer_base& operator =(dynamic_buffer_base const& rhs)
     {
         *this = dynamic_buffer_base(rhs); // copy & move for exception safety
         return *this;
@@ -226,7 +225,7 @@ public:
     {
         return data_;
     }
-    MAKESHIFT_NODISCARD constexpr const T* data(void) const noexcept
+    MAKESHIFT_NODISCARD constexpr T const* data(void) const noexcept
     {
         return data_;
     }
@@ -244,13 +243,13 @@ public:
         : data_(_size > 0 ? new T[_size]{ } : nullptr), size_(_size)
     {
     }
-    constexpr dynamic_buffer_base(const dynamic_buffer_base& rhs)
+    constexpr dynamic_buffer_base(dynamic_buffer_base const& rhs)
         : dynamic_buffer_base(rhs.size_)
     {
         // TODO: we could use non-initializing allocation and unitialized_copy() to optimize this further
         std::copy(rhs.begin(), rhs.end(), data_);
     }
-    constexpr dynamic_buffer_base& operator =(const dynamic_buffer_base& rhs)
+    constexpr dynamic_buffer_base& operator =(dynamic_buffer_base const& rhs)
     {
         *this = dynamic_buffer_base(rhs); // copy & move for exception safety
         return *this;
@@ -271,7 +270,7 @@ public:
     {
         return data_;
     }
-    MAKESHIFT_NODISCARD constexpr const T* data(void) const noexcept
+    MAKESHIFT_NODISCARD constexpr T const* data(void) const noexcept
     {
         return data_;
     }
@@ -285,62 +284,79 @@ enum class memory_location
     never_on_stack
 };
 
-static constexpr memory_location determine_memory_location(dim bufExtent, dim maxStaticBufferExtent) noexcept
+static constexpr memory_location determine_memory_location(std::ptrdiff_t bufExtent, std::ptrdiff_t maxStaticBufferExtent) noexcept
 {
-    if (maxStaticBufferExtent < 0)
-        return bufExtent < 0 ? memory_location::never_on_stack : memory_location::always_on_stack;
-    if (maxStaticBufferExtent == 0)
-        return memory_location::never_on_stack;
-    if (bufExtent < 0)
-        return memory_location::dynamic;
-    if (bufExtent <= maxStaticBufferExtent)
-        return memory_location::always_on_stack;
+    if (maxStaticBufferExtent < 0) return bufExtent < 0 ? memory_location::never_on_stack : memory_location::always_on_stack;
+    if (maxStaticBufferExtent == 0) return memory_location::never_on_stack;
+    if (bufExtent < 0) return memory_location::dynamic;
+    if (bufExtent <= maxStaticBufferExtent) return memory_location::always_on_stack;
     return memory_location::never_on_stack;
 }
 
 
-template <typename T, dim Extent, dim MaxStaticBufferExtent, memory_location MemoryLocation>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent, memory_location MemoryLocation>
     class buffer_base;
-template <typename T, dim Extent, dim MaxStaticBufferExtent>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent>
     class buffer_base<T, Extent, MaxStaticBufferExtent, memory_location::always_on_stack>
         : public static_buffer_base<T, Extent>
 {
 private:
-    using _base = static_buffer_base<T, Extent>;
+    using base_ = static_buffer_base<T, Extent>;
 
 public:
-    using _base::_base;
+    using base_::base_;
 };
-template <typename T, dim Extent, dim MaxStaticBufferExtent>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent>
     class buffer_base<T, Extent, MaxStaticBufferExtent, memory_location::dynamic>
         : public dynamic_buffer_base<T, MaxStaticBufferExtent>
 {
 private:
-    using _base = dynamic_buffer_base<T, MaxStaticBufferExtent>;
+    using base_ = dynamic_buffer_base<T, MaxStaticBufferExtent>;
 
 public:
-    using _base::_base;
+    using base_::base_;
 };
-template <typename T, dim Extent, dim MaxStaticBufferExtent>
+template <typename T, std::ptrdiff_t Extent, std::ptrdiff_t MaxStaticBufferExtent>
     class buffer_base<T, Extent, MaxStaticBufferExtent, memory_location::never_on_stack>
         : public dynamic_buffer_base<T, 0>
 {
 private:
-    using _base = dynamic_buffer_base<T, 0>;
+    using base_ = dynamic_buffer_base<T, 0>;
 
 public:
-    using _base::_base;
+    using base_::base_;
 };
 
-template <typename C>
-    constexpr dim extent_from_constval_impl(std::true_type /*isConstval*/)
-{
-    return constval_value<C>;
-}
-template <typename C>
-    constexpr dim extent_from_constval_impl(std::false_type /*isConstval*/)
+template <typename ExtentC>
+    constexpr std::ptrdiff_t buffer_extent_from_constval(ExtentC)
 {
     return -1;
+}
+template <typename T, T V>
+    constexpr std::ptrdiff_t buffer_extent_from_constval(std::integral_constant<T, V>)
+{
+    return V;
+}
+
+template <typename C>
+    constexpr inline void check_buffer_extents(std::true_type /*dynamicExtent*/, std::ptrdiff_t /*expectedExtent*/, C actualExtent)
+{
+    Expects(actualExtent >= 0);
+}
+template <typename C>
+    constexpr inline void check_buffer_extents(std::false_type /*dynamicExtent*/, std::ptrdiff_t expectedExtent, C actualExtent)
+{
+    Expects(actualExtent == expectedExtent);
+}
+template <typename C>
+    constexpr inline void check_fixed_buffer_extents(std::true_type /*dynamicExtent*/, std::ptrdiff_t /*expectedExtent*/, C actualExtent, std::ptrdiff_t maxBufferExtent)
+{
+    Expects(actualExtent >= 0 && actualExtent <= maxBufferExtent);
+}
+template <typename C>
+    constexpr inline void check_fixed_buffer_extents(std::false_type /*dynamicExtent*/, std::ptrdiff_t expectedExtent, C actualExtent, std::ptrdiff_t /*maxBufferExtent*/)
+{
+    Expects(actualExtent == expectedExtent);
 }
 
 
