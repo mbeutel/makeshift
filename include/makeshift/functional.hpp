b5c6a498daf6c028b6bb6c79c52678fc7c4a0766
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-12-19 17:24:26 +0100

    Add `[lvalue_const_ref|lvalue_ref|rvalue_ref]_fn<>`

--- include/makeshift/functional.hpp
+++ include/makeshift/functional.hpp
@@ -66,6 +66,75 @@ constexpr overloaded<Fs...> make_overloaded(Fs... fs)
 }
 
 
+template <typename F>
+struct rvalue_ref_fn : F
+{
+    constexpr rvalue_ref_fn(F func) : F(std::move(func)) { }
+    template <typename... Ts>
+    constexpr auto operator ()(Ts&&... args) &&
+        -> decltype(static_cast<F&&>(*this)(std::forward<Ts>(args)...))
+    {
+        return static_cast<F&&>(*this)(std::forward<Ts>(args)...);
+    }
+};
+#if gsl_CPP17_OR_GREATER
+template <typename F>
+rvalue_ref_fn(F) -> rvalue_ref_fn<F>;
+#endif // gsl_CPP17_OR_GREATER
+
+template <typename F>
+constexpr rvalue_ref_fn<F> make_rvalue_ref_fn(F func)
+{
+    return { std::move(func) };
+}
+
+
+template <typename F>
+struct lvalue_ref_fn : F
+{
+    constexpr lvalue_ref_fn(F func) : F(std::move(func)) { }
+    template <typename... Ts>
+    constexpr auto operator ()(Ts&&... args) &
+        -> decltype(static_cast<F&>(*this)(std::forward<Ts>(args)...))
+    {
+        return static_cast<F&>(*this)(std::forward<Ts>(args)...);
+    }
+};
+#if gsl_CPP17_OR_GREATER
+template <typename F>
+lvalue_ref_fn(F) -> lvalue_ref_fn<F>;
+#endif // gsl_CPP17_OR_GREATER
+
+template <typename F>
+constexpr lvalue_ref_fn<F> make_lvalue_ref_fn(F func)
+{
+    return { std::move(func) };
+}
+
+
+template <typename F>
+struct lvalue_const_ref_fn : F
+{
+    constexpr lvalue_const_ref_fn(F func) : F(std::move(func)) { }
+    template <typename... Ts>
+    constexpr auto operator ()(Ts&&... args) const&
+        -> decltype(static_cast<F const&>(*this)(std::forward<Ts>(args)...))
+    {
+        return static_cast<F const&>(*this)(std::forward<Ts>(args)...);
+    }
+};
+#if gsl_CPP17_OR_GREATER
+template <typename F>
+lvalue_const_ref_fn(F) -> lvalue_const_ref_fn<F>;
+#endif // gsl_CPP17_OR_GREATER
+
+template <typename F>
+constexpr lvalue_const_ref_fn<F> make_lvalue_const_ref_fn(F func)
+{
+    return { std::move(func) };
+}
+
+
     //ᅟ
     // Higher-order function for defining recursive lambda functions.
     // Note that the lambda function must explicitly declare a return type.
